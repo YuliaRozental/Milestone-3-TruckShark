26c6380987e99df9091ef242fb13a64ca4193e25
Yulia <yuyarozental@gmail.com>
2023-01-09 14:03:50 -0500

    connected a database to backend

--- trucks/__init__.py
+++ trucks/__init__.py
@@ -1,5 +1,6 @@
 #config
 from flask import Flask, render_template 
+from flask_migrate import Migrate 
 import os
 
 #factory
@@ -9,16 +10,26 @@ def create_app():
     picFolder = os.path.join('static', 'assets')
 
     app.config['UPLOAD_FOLDER'] = picFolder
+    
+    # database config
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:123qwe@localhost:5432/truckshark'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+    from . import models
+    models.db.init_app(app)
+    migrate = Migrate(app, models.db)      
 
     # index route
     @app.route("/")
     def index(): 
-        pic1 = os.path.join(app.config['UPLOAD_FOLDER'], 'paula-vermeulen-URjZkhqsuBk-unsplash.jpg')
-        return render_template("index.html", user_image = pic1)
+        return render_template("index.html")
 
     # register truck blueprint
     from . import truck
     app.register_blueprint(truck.bp)
+
+    # register fact blueprint 
+    from . import fact
+    app.register_blueprint(fact.bp)
 
     return app
