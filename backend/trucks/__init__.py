03055a96e86e9c31cc4ef23ceb5c2ef599a6c37c
Yulia <yuyarozental@gmail.com>
2023-01-14 10:16:12 -0500

    added jtw refresh token

--- backend/trucks/__init__.py
+++ backend/trucks/__init__.py
@@ -1,19 +1,18 @@
 #config
 from flask import Flask, render_template, request, jsonify
+from datetime import datetime, timedelta, timezone
 from flask_jwt_extended import create_access_token, JWTManager, get_jwt, get_jwt_identity, jwt_required, unset_jwt_cookies
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+import json
 import os
 
 #factory
 def create_app(): 
     app = Flask(__name__)
 
-    picFolder = os.path.join('static', 'assets')
-
-    app.config['UPLOAD_FOLDER'] = picFolder
-
-    app.config["JWT_SECRET_KEY"] = "please-remember"
+    app.config["JWT_SECRET_KEY"] = "chane-this"
+    app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(hours=1)
     jwt = JWTManager(app)
     
     # database config
@@ -27,8 +26,24 @@ def create_app():
     # index route
     @app.route("/")
     def index(): 
-        pic1 = os.path.join(app.config['UPLOAD_FOLDER'], 'isaac-benhesed--4K1IMX_EQc-unsplash.jpg')
-        return render_template("index.html", user_image=pic1)
+        return render_template("index.html")
+
+    @app.after_request
+    def refresh_expiring_jwts(response):
+        try:
+            exp_timestamp = get_jwt()["exp"]
+            now = datetime.now(timezone.utc)
+            target_timestamp = datetime.timestamp(now + timedelta(minutes=30))
+            if target_timestamp > exp_timestamp:
+                access_token = create_access_token(identity=get_jwt_identity())
+                data = response.get_json()
+                if type(data) is dict:
+                    data["access_token"] = access_token 
+                    response.data = json.dumps(data)
+            return response
+        except (RuntimeError, KeyError):
+        # Case where there is not a valid JWT. Just return the original respone
+            return response
 
     @app.route('/token', methods=["POST"])
     def create_token():
@@ -41,8 +56,14 @@ def create_app():
         response = {"access_token":access_token}
         return response
 
+    @app.route("/logout", methods=["POST"])
+    def logout():
+        response = jsonify({"msg": "logout successful"})
+        unset_jwt_cookies(response)
+        return response
 
     @app.route('/profile')
+    @jwt_required()
     def my_profile():
         response_body = {
             "name": "Dog",
